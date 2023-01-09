5ac032f1a794c0914b25c66a20afade01847f332
Ivan Kalashnikov <steindvart.st@gmail.com>
2023-01-09 12:05:36 +0600

    Include install sources

--- Tools/choco-win/install-all.ps1
+++ Tools/choco-win/install-all.ps1
@@ -1,4 +1,10 @@
 # Use comment (#) to exclude install
 
 Start-process powershell .\install-data.ps1
+Start-process powershell .\install-dev.ps1
+Start-process powershell .\install-security.ps1
+Start-process powershell .\install-social.ps1
 Start-process powershell .\install-web.ps1
+Start-process powershell .\install-win-common.ps1
+Start-process powershell .\install-win-dev.ps1
+Start-process powershell .\install-win-media.ps1
