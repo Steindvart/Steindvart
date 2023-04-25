19642277dfb6d406891944b28e899b7ff2250622
Ivan Kalashnikov <steindvart.st@gmail.com>
2023-04-25 19:28:32 +0600

    Added readers apps and soft sections in scripts.

--- Tools/choco-win/install-data.ps1
+++ Tools/choco-win/install-data.ps1
@@ -1,9 +1,17 @@
 # Use comment (#) to exclude install
 
+
+# Work with data
 choco install 7zip -y
-choco install obsidian -y
 choco install megasync -y
 choco install qbittorrent -y
+
+# Office and readers (pdf, epub, etc.)
 choco install thunderbird -y
 choco install onlyoffice -y
+choco install sumatrapdf -y
+choco install okular -y
+
+# Education and productivity
+choco install obsidian -y
 choco install anki -y
