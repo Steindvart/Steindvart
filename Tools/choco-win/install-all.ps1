42f24532733bf9c973673decb164aa44cf74fe93
Ivan Kalashnikov <steindvart.st@gmail.com>
2023-04-25 19:10:47 +0600

    Processed all in one terminal.

--- Tools/choco-win/install-all.ps1
+++ Tools/choco-win/install-all.ps1
@@ -1,10 +1,10 @@
 # Use comment (#) to exclude install
 
-Start-process powershell .\install-data.ps1
-Start-process powershell .\install-dev.ps1
-Start-process powershell .\install-security.ps1
-Start-process powershell .\install-social.ps1
-Start-process powershell .\install-web.ps1
-Start-process powershell .\install-win-common.ps1
-Start-process powershell .\install-win-dev.ps1
-Start-process powershell .\install-win-media.ps1
+powershell .\install-data.ps1
+powershell .\install-dev.ps1
+powershell .\install-security.ps1
+powershell .\install-social.ps1
+powershell .\install-web.ps1
+powershell .\install-win-common.ps1
+powershell .\install-win-dev.ps1
+powershell .\install-win-media.ps1
