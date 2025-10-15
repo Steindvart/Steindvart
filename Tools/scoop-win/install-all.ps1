a46e1f3ec06daf975378ba43c2e1380b075f7f0c
Ivan Kalashnikov <ivank.public@gmail.com>
2025-10-15 15:06:45 +0700

    Correct policies configure for install all from scoop.

--- Tools/scoop-win/install-all.ps1
+++ Tools/scoop-win/install-all.ps1
@@ -1,4 +1,8 @@
 # Use comment (#) to exclude install
 
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+
 powershell .\install-scoop.ps1
 powershell .\install-app.ps1
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
